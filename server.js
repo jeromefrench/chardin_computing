30064a4fcfff1188f700bb8d9c53de0496eca33a
jeromefrench <jerome.chardin@live.fr>
2020-03-21 13:42:51 +0000

    corss

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const controller = require(SRC+'/controller/index.js');
 
 app.use('/static', express.static(SRC + '/public'));
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://chardin-computing.freeboxos.fr"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://chardin-computing.freeboxos.fr"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
