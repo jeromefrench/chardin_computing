04b252bc90a8e2631ee991fad50e4cdfa17072cd
jeromefrench <jerome.chardin@live.fr>
2020-03-22 11:34:11 +0000

    process env port pour le port en back end

--- server.js
+++ server.js
@@ -20,5 +20,7 @@ app.get('/test', function (req, res) { res.send('test back'); });
 app.get('/podcast', controller.home.getPodcast);
 
 
-console.log("Server listening on port 3000");
-app.listen(3000);
+PORT = process.env.PORT | 3000;
+
+console.log("Server back listening on port " + PORT);
+app.listen(PORT);
