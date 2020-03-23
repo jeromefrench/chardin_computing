79aef1589a0595b85e761e0e6e149dc51d0a4a7c
jeromefrench <jerome.chardin@live.fr>
2020-03-23 21:09:04 +0000

    add favicon in front better gitignore

--- front/server.js
+++ front/server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 
 
+app.use(express.static(__dirname + '/public'));
+
+
+
 app.get('/test', function (req, res) { res.send('test front'); });
 app.get('/', function (req, res) { res.sendFile(views + 'home.html'); });
 app.get('/adminPodcast', function (req, res) { res.sendFile(views + 'adminPodcast.html'); });
