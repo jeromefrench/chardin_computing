a210bcf40a8eddba5765d47481f2cf90ff5cd6b6
jeromefrench <jerome.chardin@live.fr>
2020-03-25 10:27:49 +0000

    vue loader set

--- front/server.js
+++ front/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 
 
---
+++ front/views/elements/podcast.vue
@@ -0,0 +1,64 @@
+
+<template>
+	<div>
+
+<v-card
+v-for="src in podcastSrc" 
+:key="src"
+outlined
+tile
+>
+{{src}}
+</br>
+
+	<audio controls >
+		<source :src="buildSrc(src)" type="audio/mpeg">
+		Your browser does not support the audio element.
+	</audio>
+
+</v-card>
+
+	</div>
+</template>
+
+
+<script charset="utf-8">
+
+
+module.exports = {
+//var app = new Vue({
+	//vuetify: new Vuetify(),
+	data: function() {
+		return {
+			podcastSrc: null,
+			showPodcast: false,
+		}
+	},
+	methods: {
+		getPodcast(){
+			axios.get('http://chardin-computing.freeboxos.fr:3000/podcast', {
+			})
+			.then((response)=> {
+				this.podcastSrc = response.data;
+				console.log(this.podcastSrc);
+				console.log(this.showPodcast);
+				this.showPodcast = true;
+				console.log(this.showPodcast);
+				console.log("hello");
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+		},
+		buildSrc(src){
+			return "http://chardin-computing.freeboxos.fr:3000/static/podcast/" + src;
+		}
+	},
+	mounted: function() {
+		this.getPodcast();
+	}
+//})
+}
+
+
+</script>
