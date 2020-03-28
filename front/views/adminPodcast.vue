e6a253ea2420a8539bf158ca185383d3b8c5e800
jeromefrench <jerome.chardin@live.fr>
2020-03-28 10:38:29 +0000

    validation front rules

--- front/views/adminPodcast.vue
+++ front/views/adminPodcast.vue
@@ -1,8 +1,6 @@
 <template>
 
-
-
-	<v-form v-model="valid"  class="red lighten-4 ma-8 pa-9  font-weight-medium center" style="text-align:center" >
+	<v-form v-model="valid" :lazy-validation="lazy" class="red lighten-4 ma-8 pa-9  font-weight-medium center" style="text-align:center" >
 
 		<v-card class="ma-2  pa-2 red lighten-5  ">
 			<audio controls  v-if="showPodcast"  class="ma-auto" style="width: 100%">
@@ -12,22 +10,31 @@
 			<v-file-input class="px-5"  v-model="filePodcast" @change="fileChange($event)" label="File input"></v-file-input>
 		</v-card>
 
+		<v-card class="ma-2   red lighten-5">
+			<v-text-field
+			class="px-5"
+			v-model="title"
+			:rules="[ titleRules ]"
+			label="Title"
+			></v-text-field>
+		</v-card>
 
 		<v-card class="ma-2   red lighten-5">
 			<v-text-field
 			class="px-5"
 			light
 			v-model="pathName"
+			:rules="[ pathNameRules ]"
 			label="Path name"
 			></v-text-field>
 		</v-card>
 
-
 		<v-card class="ma-2 pa-2  red lighten-5">
 			<v-menu
-			v-model="menu2"
+			v-model="datePick"
 			:close-on-content-click="false"
 			:nudge-right="40"
+			:rules="[ dateRules ]"
 			transition="scale-transition"
 			offset-y
 			min-width="290px"
@@ -42,18 +49,17 @@
 					v-on="on"
 					></v-text-field>
 				</template>
-				<v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+				<v-date-picker v-model="date" @input="datePick = false"></v-date-picker>
 
 			</v-menu>
 		</v-card>
-
-
 
 		<v-card class="ma-2   red lighten-5">
 			<v-text-field
 			class="px-5"
 			v-model="country"
 			label="Country"
+			:rules="[ countryRules ]"
 			></v-text-field>
 		</v-card>
 
@@ -62,16 +68,16 @@
 			class="ma-2"
 			name="description"
 			label="Description"
+			v-model="description"
 			></v-textarea>
 		</v-card>
 
-
-			<v-btn
-			color="success"
-			@click="validate"
-			>
-			Validate
-			</v-btn>
+		<v-btn
+		color="success"
+		@click="validate"
+		>
+		Validate
+		</v-btn>
 
 	</v-form>
 
@@ -82,19 +88,32 @@
 module.exports = {
 	data: function() {
 			return {
-				pathName: null,
-				date: new Date().toISOString().substr(0, 10),
-				country: null,
-				menu: false,
-				modal: false,
-				menu2: false,
+				showPodcast: false,
+				datePick: false,
 				filePodcast: null,
 				audioSrc: null,
-				showPodcast: false,
+				title: '',
+				pathName: '',
+				date: new Date().toISOString().substr(0, 10),
+				country: '',
+				description: '',
 				valid: false,
+				lazy: false,
 			}
 	},
 	methods: {
+		titleRules(value){
+			return value.length == 0 ? 'field required': true;
+		},
+		pathNameRules(value){
+			return value.length == 0 ? 'field required': true;
+		},
+		dateRules(value){
+			return value.length == 0 ? 'field required': true;
+		},
+		countryRules(value){
+			return value.length == 0 ? 'field required': true;
+		},
 		fileChange($event){
 			this.audioSrc = URL.createObjectURL(this.filePodcast);
 			this.showPodcast = true;
