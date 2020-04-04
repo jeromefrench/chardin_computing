f219ed92529d1f9c8bb16e04ce8b6827bae293f0
jeromefrench <jerome.chardin@live.fr>
2020-04-04 10:55:59 +0100

    production and development folder

--- development/utils/middleware/server.js
+++ development/utils/middleware/server.js
@@ -11,7 +11,8 @@ var bodyParser = require('body-parser');
 middleware = {
 
 	cors: function(req, res, next){
-		res.header("Access-Control-Allow-Origin", "http://chardin-computing.freeboxos.fr");
+		res.header("Access-Control-Allow-Origin", "http://chardin-computing.freeboxos.fr:8081");
+		res.header("Access-Control-Allow-Origin", "http://localhost:8080");
 		res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		next();
