8ff26d4150297bb42c2e645a43355892f5f4024f
jeromefrench <jerome.chardin@live.fr>
2020-04-07 10:57:32 +0100

    deployment

--- back_deployement.sh
+++ back_deployement.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 set -x
 
 CURRENT=$PWD
+#copy server
 cp -r "$CURRENT/development/back/"* "$CURRENT/production/back/"
+
+#launch server
 (cd $CURRENT/production/back ; /usr/bin/concurrently "/usr/bin/node $CURRENT/production/back/server.js")
