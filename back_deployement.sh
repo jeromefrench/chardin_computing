c9388b6380250261fb144c2d107d4284c5766e52
jeromefrench <jerome.chardin@live.fr>
2020-04-09 12:44:15 +0100

    change path for back

--- back_deployement.sh
+++ back_deployement.sh
@@ -5,16 +5,16 @@ set -x
 CURRENT=$PWD
 
 #install modules
-(cd $CURRENT/development/back ; npm install)
+(cd $CURRENT/server_back ; npm install)
 
 #create direcories if not exist
-mkdir -p "$CURRENT/production/back"
+mkdir -p "$CURRENT/production/server_back"
 
 #copy env back
-cp  "$CURRENT/env_back" "$CURRENT/production/back/.env"
+cp  "$CURRENT/env_back" "$CURRENT/production/server_back/.env"
 
 #copy server
-cp -r "$CURRENT/development/back/"* "$CURRENT/production/back/"
+cp -r "$CURRENT/server_back/"* "$CURRENT/production/server_back/"
 
 #launch server
-(cd $CURRENT/production/back ; /usr/bin/concurrently "/usr/bin/node $CURRENT/production/back/server.js")
+(cd $CURRENT/production/server_back ; /usr/bin/concurrently "/usr/bin/node $CURRENT/production/server_back/server.js")
